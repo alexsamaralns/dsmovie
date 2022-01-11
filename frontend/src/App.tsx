fd26b67147d209be2221d6094a6d709847171276
alexsamaralns <alexsamaralns@gmail.com>
2022-01-10 22:56:45 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { ReactComponent as GitHubIcon } from './assets/img/github.svg';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1>Página principal</h1>
+    <Navbar />
   );
 }
 
